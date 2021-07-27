41cd554d8bd3b7c0e8f91ddb78054321f44a360f
Leninner <mazabandalenin180@gmail.com>
2021-07-27 14:43:15 -0500

    Añado funcionalidad para mostrar los títulos a las cajas de elección

--- src/gameController.js
+++ src/gameController.js
@@ -9,18 +9,20 @@ import virus from "./images/virus.jpg";
 import { doTable } from "./tableMain.js";
 
 let goodPractices = {
-    1: { imagen: distancia, frase: "Esto es bueno" },
-    2: { imagen: manos, frase: "Manitos" },
+    1: { imagen: distancia, frase: "Mantener Distancia" },
+    2: { imagen: manos, frase: "Lavarse las Manos" },
     3: { imagen: mascarilla, frase: "Usar mascarilla" },
     4: { imagen: saludable, frase: "Comer saludable" },
 };
 
 let badPractices = {
-    1: { imagen: aglomeracion, frase: "Esto es malo" },
-    2: { imagen: virus, frase: "Esto es re malo" },
-    3: { imagen: sinMacarilla, frase: "Es malo" },
-    4: { imagen: chatarra, frase: "Esto es bueno" },
+    1: { imagen: aglomeracion, frase: "Estar todos unidos" },
+    2: { imagen: virus, frase: "Covid - 19" },
+    3: { imagen: sinMacarilla, frase: "No usar mascarilla" },
+    4: { imagen: chatarra, frase: "Comida Chatarra" },
 };
+
+//TODO: Hacer titulos de cada imagen para que el niño haga su elección
 
 function getWinner() {
     const itemOne = document.querySelector(".divItemOne"),
@@ -29,29 +31,40 @@ function getWinner() {
     let imgOne = document.createElement("img"),
         imgTwo = document.createElement("img");
 
+    let divImgOne = document.createElement("div"),
+        divImgTwo = document.createElement("div");
+
+    let titleImgOne = document.createElement("h3"),
+        titleImgTwo = document.createElement("h3");
+
+    titleImgOne.textContent = goodPractices[1].frase;
+    titleImgTwo.textContent = badPractices[1].frase;
+
     imgOne.alt = "Esta es una buena imagen";
     imgOne.src = goodPractices[1].imagen;
-    itemOne.appendChild(imgOne);
+    divImgOne.append(titleImgOne, imgOne);
+    itemOne.appendChild(divImgOne);
 
     imgTwo.alt = "Esta es una mala imagen";
     imgTwo.src = badPractices[1].imagen;
-    itemTwo.appendChild(imgTwo);
+    divImgTwo.append(titleImgTwo, imgTwo);
+    itemTwo.appendChild(divImgTwo);
 
     let cont = 2;
 
     itemOne.addEventListener("click", () => {
         if (cont == 2) {
-            setItemOne(imgOne, 2);
-            setItemTwo(imgTwo, 2);
+            setItemOne(imgOne, 2, titleImgOne);
+            setItemTwo(imgTwo, 2, titleImgTwo);
         } else if (cont == 3) {
-            setItemOne(imgOne, 3);
-            setItemTwo(imgTwo, 3);
+            setItemOne(imgOne, 3, titleImgOne);
+            setItemTwo(imgTwo, 3, titleImgTwo);
         } else if (cont == 4) {
-            setItemOne(imgOne, 4);
-            setItemTwo(imgTwo, 4);
+            setItemOne(imgOne, 4, titleImgOne);
+            setItemTwo(imgTwo, 4, titleImgTwo);
         } else {
-            setItemOne(imgOne, 1);
-            setItemTwo(imgTwo, 1);
+            setItemOne(imgOne, 1, titleImgOne);
+            setItemTwo(imgTwo, 1, titleImgTwo);
             cont = 1;
         }
 
@@ -60,17 +73,17 @@ function getWinner() {
 
     itemTwo.addEventListener("click", () => {
         if (cont == 2) {
-            setItemOne(imgOne, 2);
-            setItemTwo(imgTwo, 2);
+            setItemOne(imgOne, 2, titleImgOne);
+            setItemTwo(imgTwo, 2, titleImgTwo);
         } else if (cont == 3) {
-            setItemOne(imgOne, 3);
-            setItemTwo(imgTwo, 3);
+            setItemOne(imgOne, 3, titleImgOne);
+            setItemTwo(imgTwo, 3, titleImgTwo);
         } else if (cont == 4) {
-            setItemOne(imgOne, 4);
-            setItemTwo(imgTwo, 4);
+            setItemOne(imgOne, 4, titleImgOne);
+            setItemTwo(imgTwo, 4, titleImgTwo);
         } else {
-            setItemOne(imgOne, 1);
-            setItemTwo(imgTwo, 1);
+            setItemOne(imgOne, 1, titleImgOne);
+            setItemTwo(imgTwo, 1, titleImgTwo);
             cont = 1;
         }
 
@@ -78,14 +91,16 @@ function getWinner() {
     });
 }
 
-function setItemOne(image, number) {
+function setItemOne(image, number, title) {
     image.alt = "Esta es una buena imagen";
     image.src = goodPractices[number].imagen;
+    title.textContent = goodPractices[number].frase;
 }
 
-function setItemTwo(image, number) {
+function setItemTwo(image, number, title) {
     image.alt = "Esta es una mala imagen";
     image.src = badPractices[number].imagen;
+    title.textContent = badPractices[number].frase;
 }
 
 function setElements() {
